6e4b32e84fc09aeadb99703fb96adacdd77cd2f9
Medios Con Valor <sistemas@mediosconvalor.com>
2025-06-24 16:27:30 -0600

    Update main.js

--- script/portal/main.js
+++ script/portal/main.js
@@ -2,7 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
   const btnOpen  = document.querySelector('.hamburger');
   const btnClose = document.querySelector('.sidebar__close');
   const sidebar  = document.querySelector('.sidebar');
+  const body     = document.body;
 
-  btnOpen.addEventListener('click',  () => sidebar.classList.add('open'));
-  btnClose.addEventListener('click', () => sidebar.classList.remove('open'));
+  btnOpen.addEventListener('click', () => {
+    sidebar.classList.add('open');
+    body.classList.add('sidebar-open');
+  });
+
+  btnClose.addEventListener('click', () => {
+    sidebar.classList.remove('open');
+    body.classList.remove('sidebar-open');
+  });
 });
